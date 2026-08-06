fca46e1000f2307b16f563a54d9488c2f8871270
MyNameHuda <nrlhuda2904@gmail.com>
2026-08-06 07:23:25 +0700

    chore: remove temp clean script

--- _clean2.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const file = 'C:/Users/bangn/Documents/Kerja/lomba/lomba-app/test-v4-system.cjs';
-let c = fs.readFileSync(file, 'utf8');
-// Remove trailing whitespace including non-breaking spaces and BOM
-c = c.replace(/[\s\u00A0\u200B-\u200D\uFEFF]+$/, '\n');
-fs.writeFileSync(file, c, 'utf8');
-console.log('Trimmed, length:', c.length);
